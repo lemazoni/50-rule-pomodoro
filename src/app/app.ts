2788d66e958b8278db1358d6dedfd79f66e06943
Leandro <mazoni.le@gmail.com>
2026-04-18 17:52:09 -0300

    remove main page, add router and materials

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardImage, MatCardSubtitle, MatCardTitle } from '@angular/material/card';
+import { MatButton } from '@angular/material/button';
+import { MainPage } from './main-page/main-page';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('50-rule-pomodoro');
